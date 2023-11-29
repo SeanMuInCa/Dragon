fc6c7a7c5574233fa3a919fd5e92af03a89e2c4f
Sean <fengying1028@hotmail.com>
2023-11-28 20:33:59 -0600

    finished the sort part, from the idea of online

--- GameTest.java
+++ GameTest.java
@@ -18,15 +18,15 @@ public class GameTest
 {
     public static void main(String[] args)
     {
-        ArrayList<Dragon> list = initDragon();
-        int[] arr = new int[list.size()];
+        ArrayList<Dragon> list = initDragon();//store dragon
+        int[] arr = new int[list.size()];//store wins
         int rounds = list.size() - 1;
         int matchesPerRound = list.size() / 2;
         for (int i = 0; i < rounds; i++)
         {
             for (int j = 0; j < matchesPerRound; j++)
             {
-                Dragon dragon = battleRound(list.get(j),list.get(list.size() - 1 - j));
+                Dragon dragon = battleRound(list.get(j), list.get(list.size() - 1 - j));
                 arr[list.indexOf(dragon)]++;
             }
             for (Dragon dragon : list)
@@ -37,84 +37,116 @@ public class GameTest
         }
         System.out.println(list);
         System.out.println(Arrays.toString(arr));
+        //store the index
+        int[] indexes = new int[arr.length];
+        for (int i = 0; i < indexes.length; i++) {
+            indexes[i] = i;
+        }
+        //sort the index based on arr
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[indexes[j]] < arr[indexes[j + 1]]) {
+                    int temp = indexes[j];
+                    indexes[j] = indexes[j + 1];
+                    indexes[j + 1] = temp;
+                }
+            }
+        }
+//put the elements into a new arraylist based on the order of the index array
+        ArrayList<Dragon> result = new ArrayList<>();
+        for (int i = 0; i < indexes.length; i++) {
+            result.add(list.get(indexes[i]));
+        }
+        System.out.println(list);
+        System.out.println(result);
     }
+
     public static ArrayList<Dragon> initDragon()
     {
         ArrayList<Dragon> list = new ArrayList<>();
-        for (int i = 0; i < 3; i++)
+        for (int i = 1; i <= 3; i++)
         {
             //generate 4 larges
-            list.add(new Red(Dragon.DragonSize.Large, "Red"));
-            list.add(new Green(Dragon.DragonSize.Large, "Green"));
-            list.add(new White(Dragon.DragonSize.Large, "White"));
-            list.add(new Black(Dragon.DragonSize.Large, "Black"));
+            list.add(new Red(Dragon.DragonSize.Large, "Red" + i));
+            list.add(new Green(Dragon.DragonSize.Large, "Green" + i));
+            list.add(new White(Dragon.DragonSize.Large, "White" + i));
+            list.add(new Black(Dragon.DragonSize.Large, "Black" + i));
             //generate 4 Medium
-            list.add(new Red(Dragon.DragonSize.Medium, "Red"));
-            list.add(new Green(Dragon.DragonSize.Medium, "Green"));
-            list.add(new White(Dragon.DragonSize.Medium, "White"));
-            list.add(new Black(Dragon.DragonSize.Medium, "Black"));
+            list.add(new Red(Dragon.DragonSize.Medium, "Red" + i));
+            list.add(new Green(Dragon.DragonSize.Medium, "Green" + i));
+            list.add(new White(Dragon.DragonSize.Medium, "White" + i));
+            list.add(new Black(Dragon.DragonSize.Medium, "Black" + i));
             //generate 4 Small
-            list.add(new Red(Dragon.DragonSize.Small, "Red"));
-            list.add(new Green(Dragon.DragonSize.Small, "Green"));
-            list.add(new White(Dragon.DragonSize.Small, "White"));
-            list.add(new Black(Dragon.DragonSize.Small, "Black"));
+            list.add(new Red(Dragon.DragonSize.Small, "Red" + i));
+            list.add(new Green(Dragon.DragonSize.Small, "Green" + i));
+            list.add(new White(Dragon.DragonSize.Small, "White" + i));
+            list.add(new Black(Dragon.DragonSize.Small, "Black" + i));
         }
         return list;
     }
 
     public static Dragon battleRound(Dragon obDragon1, Dragon obDragon2)
     {
-        while (true){
+        while (true)
+        {
             int times1 = obDragon1.getNumAttacksPerTurn();
             int times2 = obDragon2.getNumAttacksPerTurn();
+            int random = Math.random() < 0.5 ? 1 : 2;
             System.out.println(obDragon1 + " vs " + obDragon2);
-            if(obDragon1.getInitiative() > obDragon2.getInitiative()){
-//                for(;times1>0 || times2 > 0;times1--,times2--){
-                    if(attack(obDragon1, obDragon2, times1, times2)) break;
-//                }
-                if(obDragon1.isDead() || obDragon2.isDead()) break;
-            }else if(obDragon1.getInitiative() <= obDragon2.getInitiative()){
-//                for(;times1>0 || times2 > 0;times1--,times2--){
-                    if(attack(obDragon1, obDragon2, times1, times2)) break;
-//                }
-                if(obDragon1.isDead() || obDragon2.isDead()) break;
-            }else{
-                int random = Math.random() < 0.5 ? 1 : 2;
-                if (random == 1) {
-                    System.out.println("随机选择处理对象1");
-                } else {
-                    System.out.println("随机选择处理对象2");
+            if (obDragon1.getInitiative() > obDragon2.getInitiative())
+            {
+                if (attack(obDragon1, obDragon2, times1, times2)) break;
+            } else if (obDragon1.getInitiative() < obDragon2.getInitiative())
+            {
+                if (attack(obDragon1, obDragon2, times1, times2)) break;
+            } else
+            {
+                if (random == 1)
+                {
+                    if (attack(obDragon1, obDragon2, times1, times2)) break;
+                } else
+                {
+                    if (attack(obDragon1, obDragon2, times1, times2)) break;
                 }
             }
         }
         return obDragon1.isDead() ? obDragon2 : obDragon1;
     }
-    private static boolean attack(Dragon d1, Dragon d2, int i, int j){
-        for(;i>0 || j > 0;i--,j--)
+
+    private static boolean attack(Dragon d1, Dragon d2, int i, int j)
+    {
+        for (; i > 0 || j > 0; i--, j--)
         {
             if (i > 0)
             {
-                if(isHit(d1)){
+                if (isHit(d1))
+                {
                     d1.defendAttack(d2);
                 }
-                if (d2.isDead()) return true;
+                if (d1.isDead()) return true;
             }
-            if(j > 0) {
-                if(isHit(d2)){
+            if (j > 0)
+            {
+                if (isHit(d2))
+                {
                     d2.defendAttack(d1);
                 }
-                if(d1.isDead()) return true;
+                if (d2.isDead()) return true;
             }
         }
         return false;
     }
-    public static boolean isHit(Dragon d){
-        switch (d.nSize){
+
+    public static boolean isHit(Dragon d)
+    {
+        switch (d.nSize)
+        {
             case Small ->
             {
                 return Math.random() < 0.7;
             }
-            case Medium -> {
+            case Medium ->
+            {
                 return Math.random() < 0.8;
             }
             default ->
